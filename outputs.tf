bb26ed1f58a5de29202706308d56137f490d798b
Juan Deyvis Espinoza Santiago <107270988+juan-espinoza-s@users.noreply.github.com>
2023-01-05 15:20:48 -0500

    update module terraform

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,16 @@
 output "organization_name" {
-  value = tfe_organization.main_organization[*].name
+  value       = tfe_organization.main_organization[*].name
+  description = "Esta salida mostrará los nombres o el nombre de la organización que crearás"
 }
 output "organization_id" {
-  value = tfe_organization.main_organization[*].id
+  value       = tfe_organization.main_organization[*].id
+  description = "Esta salida mostrará los ids o el id de la organización que crearás"
 }
 output "workspace_name" {
-  value = tfe_workspace.this_workspace.name
+  value       = tfe_workspace.this_workspace.name
+  description = "Esta salida mostrará el nombre del workspace creado"
+}
+output "workspace_id" {
+  value       = tfe_workspace.this_workspace.id
+  description = "Esta salida mostrará el nombre del workspace creado"
 }
