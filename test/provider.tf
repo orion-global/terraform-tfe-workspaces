e21fd705d3349fa9bfd85e07d1a726510112c45d
Juan Deyvis Espinoza Santiago <107270988+juan-espinoza-s@users.noreply.github.com>
2023-01-05 17:23:04 -0500

    Update provider.tf

--- test/provider.tf
+++ test/provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  cloud {
-    organization = "orion-global"
-
-    workspaces {
-      name = "internal-terraform-modules" # Always use this workspace name for module creation and test
-    }
-  }
-}
 
 provider "tfe" {
   hostname = "app.terraform.io"
